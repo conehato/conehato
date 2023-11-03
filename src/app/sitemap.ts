56afdf5bcd036bb9a5dc914eebe5e0aa1c22c2ac
Junseo Oh <40460655+outsung@users.noreply.github.com>
2023-11-03 16:55:09 +0900

    Feat: update `sitemap.ts`

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,3 +1,4 @@
+import { getArticles } from "@/services/article";
 import { getRootCategories } from "@/services/category";
 import { MetadataRoute } from "next";
 
@@ -5,9 +6,20 @@ const URL = "https://conehato.com";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const categories = (await getRootCategories()).rows;
+  const articles = (await getArticles({ page: 1, limit: 4900 })).rows;
+
   const categoriesSiteMap = categories.map<MetadataRoute.Sitemap[number]>(
     (category) => ({
       url: `${URL}/${category.id}`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7,
+    })
+  );
+
+  const articlesSiteMap = articles.map<MetadataRoute.Sitemap[number]>(
+    (article) => ({
+      url: `${URL}/${article.category._id}/${article.id}`,
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.8,
@@ -22,5 +34,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
     },
     ...categoriesSiteMap,
+    ...articlesSiteMap,
   ];
 }
