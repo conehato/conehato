65f6ca8cf337b2f5a3e9b3746b82bc75d8929715
Taegyu Lee <meleeisdeveloping@gmail.com>
2023-12-04 13:13:39 +0000

    versel build error

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: process.env.NODE_ENV === 'development' ? {
+const nextConfig = process.env.NODE_ENV === 'development' ? {
+  experimental: {
     serverActions: {
       allowedForwardedHosts: ['shiny-happiness-q6v546ggpx62vj4-3000.app.github.dev'],
     },
-  } : null,
+  },
   transpilePackages: ["@ckeditor/ckeditor5-build-classic"],
   images: {
     remotePatterns: [
@@ -14,7 +14,17 @@ const nextConfig = {
       },
     ],
   },
-};
+} : {
+  transpilePackages: ["@ckeditor/ckeditor5-build-classic"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: `${process.env.IMAGE_URL_PREFIX}.public.blob.vercel-storage.com`,
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig;
 
